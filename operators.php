95dba2151cbcbd533acbec28ff17161ee696264e
Aditya <aadi099012@gmail.com>
2024-11-17 16:05:41 +0530

    operators in php

---
+++ operators.php
@@ -0,0 +1,19 @@
+<?php
+
+
+
+
+?>
+
+<!-- operators -->
+
+<!-- 
+1= arithmatic operators + - *  / % ** ;
+2= comparison operators == != === !== > < >= <= <>(not equal to) <=>(spaceship operator it return the 3 values -1,0,1  it introduce in php7 
+ if a is greater than b then it return +1
+ if a is less than b then it return -
+ if a is equal to b then it return 0
+
+) ;
+3= logical operators && || ! ;
+4= assignment operators = += -= *= /= %= **=  ; -->
