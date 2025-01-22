ecde22c3d5eb63e34250f2eaeed79738be2eb9f4
Aditya <aadi099012@gmail.com>
2025-01-23 00:57:21 +0530

    connection change

--- php-mysql.php
+++ php-mysql.php
@@ -10,6 +10,28 @@ if ($conn->connect_error) {
 }
 echo "<h1>Connection Succesful</h1>";
 
-$result = $conn->query("select * from students")->fetch_all();
-echo"<pre>";
-print_r($result);
+$getStudent = $conn->prepare("SELECT * FROM students ");
+$getStudent->execute();
+$students = $getStudent->get_result();
+
+
+echo"<Table border = '1'>";
+echo"<tr>
+<td>Name</td>
+<td>Course</td>
+<td>Batch</td>
+<td>City</td>
+<td>Year</td>
+</tr>";
+foreach($students as $std)
+{
+  echo"<tr>
+  <td>".$std['name']."</td>
+  <td>".$std['course']."</td>
+  <td>".$std['batch']."</td>
+  <td>".$std['city']."</td>
+  <td>".$std['year']."</td>
+  <tr>";
+}
+echo"</Table>";
+?>
